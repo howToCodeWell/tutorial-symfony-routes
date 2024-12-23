4be86925017a85523c286b9e421ad4a46672454c
pfwd <hello@websomatic.co.uk>
2024-12-23 10:32:12 +0000

    Adding tutorial 3

---
+++ tutorials/tutorial_3/README.md
@@ -0,0 +1,4 @@
+# Tutorial 3: Symfony route parameter requirements
+
+- /@pfwd
+- /edit
---
+++ tutorials/tutorial_3/src/Controller/ProfileController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/{userHandle}', name: 'app_profile', requirements: ['userHandle' => '^@[a-z]+'])]
+    public function index(string $userHandle): JsonResponse
+    {
+        return $this->json([
+            'userHandle' => $userHandle
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_edit_profile')]
+    public function edit(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Edit actions'
+        ]);
+    }
+}
